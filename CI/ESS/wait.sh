a786b785f8c8cc87055f43d3791337fa8651cb0f
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-26 13:16:46 +0200

    move createIndex to after sleep 20

--- CI/ESS/wait.sh
+++ CI/ESS/wait.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-mongo --host mongodb dacat createIndex.js
+echo "sleep for 20"
 sleep 20
+echo "connect via mongo"
+mongo --host mongodb dacat createIndex.js
+echo "starting npm test"
 npm test
