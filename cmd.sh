746e4f09023e3bfaa4e04677b5785177d1231084
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-29 11:47:24 +0100

    add new run script

--- cmd.sh
+++ cmd.sh
@@ -2,7 +2,7 @@
 
     DOCKERFILE="-f ./docker-compose.yaml
 if [ "$(hostname)" == "kubetest01.dm.esss.dk" ]; then
-    DOCKERFILE="-f ./CI/ESS/docker-compose.yaml
+    DOCKERFILE="-f ./CI/ESS/docker-compose.yaml"
 fi
 docker-compose ${DOCKERFILE} build --no-cache --force-rm
 docker-compose  ${DOCKERFILE} run catamel ./wait.sh --verbose
