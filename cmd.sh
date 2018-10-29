99cd362089b8502c004bd449e1a8a3aa4388b70c
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-29 14:54:03 +0100

    correct username in settings

--- cmd.sh
+++ cmd.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-    DOCKERFILE="-f ./docker-compose.yaml"
+DOCKERFILE="-f ./docker-compose.yaml"
 if [ "$(hostname)" == "kubetest01.dm.esss.dk" ]; then
     DOCKERFILE="-f ./docker-compose.proxy.yaml"
 fi
+docker-compose ${DOCKERFILE} down -v --rmi all
 docker-compose ${DOCKERFILE} build --no-cache --force-rm
 docker-compose  ${DOCKERFILE} run catamel ./wait.sh --verbose
