9cf4b54594d69533bf8d37b1244f788194f1492c
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-29 11:47:52 +0100

    add new run script

--- cmd.sh
+++ cmd.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-    DOCKERFILE="-f ./docker-compose.yaml
+    DOCKERFILE="-f ./docker-compose.yaml"
 if [ "$(hostname)" == "kubetest01.dm.esss.dk" ]; then
     DOCKERFILE="-f ./CI/ESS/docker-compose.yaml"
 fi
