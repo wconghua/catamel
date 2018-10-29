c06aac9a84ea899a7a388e5440c031a28f318bef
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-29 11:45:03 +0100

    add local config for vm

---
+++ cmd.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+    DOCKERFILE="-f ./docker-compose.yaml
+if [ "$(hostname)" == "kubetest01.dm.esss.dk" ]; then
+    DOCKERFILE="-f ./CI/ESS/docker-compose.yaml
+fi
+docker-compose ${DOCKERFILE} build --no-cache --force-rm
+docker-compose  ${DOCKERFILE} run catamel ./wait.sh --verbose
