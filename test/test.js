b113880c41747dc1d419935a22ab9ba23abc32fa
Gareth Murphy <garethcmurphy@gmail.com>
2018-11-07 14:19:32 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,9 +3,12 @@ var loopbackApiTesting = require('./testGenerator');
 var tests = require('./config/tests.json');
 var config = require('./config/settings.json');
 var server = require('../server/server.js');
- 
-loopbackApiTesting.run(tests, server, config, function(err) {
-  if (err) {
-    console.log(err);
-  }
-});
+
+
+setTimeout(() => {
+    loopbackApiTesting.run(tests, server, config, function(err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+}, 5000);
